98ba87018142ce60b1e9916e608125072941c6aa
Sadikhali-pv <sadikhalipv@gmail.com>
2025-02-06 12:26:42 +0530

    verceld

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './redux/store';
+
 import App from './App.jsx'
+import { Toaster } from './components/ui/toaster.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+      <Toaster />
+    </PersistGate>
+  </Provider>
   </StrictMode>,
 )
